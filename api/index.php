a403637f3e2b8d84842afa95596036cb60cf1554
zeiasoroush <zeia.soroush@gmail.com>
2024-04-30 22:43:26 +0330

    complete1

--- api/index.php
+++ api/index.php
@@ -1,1 +1,50 @@
 
+<?php
+
+if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
+    http_response_code(403);
+    exit('Access Forbidden - HTTPS is required.');
+}
+
+$server_domain = "https://hiddify-sub-only-domain.com";
+
+$domain = $_SERVER['HTTP_HOST'];
+$ip = $_SERVER['HTTP_CF_CONNECTING_IP'] ?? $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'];
+$url = $server_domain . $_SERVER['REQUEST_URI'];
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+$ch = curl_init();
+curl_setopt_array($ch, [
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYHOST => 2,
+    CURLOPT_HEADERFUNCTION => function($curl, $header) {
+        header($header);
+        return strlen($header);
+    },
+]);
+
+$headers = [
+    "CF-Connecting-IP: $ip",
+    "Host: $domain",
+    "User-Agent: $userAgent",
+];
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, 'HTTP_') === 0) {
+        $header = str_replace('_', '-', substr($key, 5));
+        $headers[] = "$header: $value";
+    }
+}
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$response = curl_exec($ch);
+if (curl_errno($ch)) {
+    http_response_code(500);
+    exit('Internal Server Error');
+}
+
+curl_close($ch);
+echo $response;
+
+?>
